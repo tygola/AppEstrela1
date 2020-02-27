de8e1329528f4a3cb86c3ff2e9a7f84a3ee2cb6e
tiago.cscamargo <tweedleti@gmail.com>
2020-02-27 12:11:27 -0300

    projeto estrela1

---
+++ app/src/main/java/br/com/sp/senac/appestrela/menu_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.appestrela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+    }
+}
---
+++ app/src/main/java/br/com/sp/senac/appestrela/splash_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.appestrela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+    }
+}
